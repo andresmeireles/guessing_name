35afa0350bde59625cfd6cfd651c5d621d59ca18
André Meireles <andre2meireles@gmail.com>
2021-12-24 17:04:41 -0300

    finish guessing game

---
+++ README.md
@@ -0,0 +1,33 @@
+# Guess game
+
+## how it works
+
+A simple console guess the number game.
+
+## how build
+
+Ensure that you have rust installed on your machine
+
+First clone this repository with command
+
+`https://github.com/andresmeireles/guessing_name.git`
+
+After go to source folder with command
+
+`cd guessing_game`
+
+In source code directory run command
+
+`cargo run`
+
+And play!
+
+## Alternatively
+
+You can build this little game with command:
+
+`cargo build --release`
+
+The project will be build on `target/release/guessing_game`, put this binary on any pendrive and play this game on every pc
+
+:)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("guess a number!");
-    println!("please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        println!("please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
